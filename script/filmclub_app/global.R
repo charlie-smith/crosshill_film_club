2545112603994ae4d5b3b753d5dca5f7b5d34e66
charlie-smith <charlie.smith2@phs.scot>
2023-11-15 18:00:18 +0000

    test to see if anything bad goes on github

--- script/filmclub_app/global.R
+++ script/filmclub_app/global.R
@@ -16,4 +16,11 @@ library(shinymanager)
 #           scopes = "drive.file")
 
 
-googledrive::drive_auth_configure()
+
+
+# gcloud: 
+# https://console.cloud.google.com/iam-admin/serviceaccounts/details/115676623624084979748;edit=true/keys?project=crosshill-film-club
+
+# Help:
+# https://stackoverflow.com/questions/62768419/r-googlesheets4-non-interactive-session
+# https://stackoverflow.com/questions/65051305/struggling-to-use-my-own-api-key-with-googlesheets4-in-shinyapps-io
